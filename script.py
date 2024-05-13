6190bd190b5aa477a61378a033fa44441ad7ebba
DEV-Devound <88888648+DEV-Devound@users.noreply.github.com>
2024-05-13 18:43:17 -0400

    update

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, send_from_directory
 from werkzeug.utils import secure_filename
-import photoshop.api as ps
+from photoshop import Session
 import os
 
 app = Flask(__name__, static_url_path='')
@@ -11,40 +11,46 @@ def index():
 
 @app.route('/write', methods=['POST'])
 def write():
-    # Request first name, last name and psd file from React
+    # Request first_name, last_name, psd_file and profile_image from React application
     ## Example
-    #first_name = request.form['first_name']
-    #last_name = request.form['last_name']
-    #psd_file = request.files['psd_file']
-    #profile_image = request.files['profile_image']
+    ###first_name = request.form['first_name']
+    ###last_name = request.form['last_name']
+    ###psd_file = request.files['psd_file']
+    ###profile_image = request.files['profile_image']
 
-    psd_file_path = os.path.abspath(secure_filename(psd_file.filename))
-    #profile_image_path = os.path.abspath(secure_filename(profile_image.filename))
+    ###psd_file_path = os.path.abspath(secure_filename(psd_file.filename))
+    ###profile_image_path = os.path.abspath(secure_filename(profile_image.filename))
 
-    psd_file.save(psd_file_path)
+    ###psd_file.save(psd_file_path)
     #profile_image.save(profile_image_path)
 
     # Load the PSD file
-    doc = ps.Application().open(psd_file_path)
+    with Session(psd_file_path, action="open", auto_close=True) as ps:
+        doc = ps.active_document
+        desc = ps.ActionDescriptor
+        desc.putPath(ps.app.charIDToTypeID("null"), profile_image_path)
+        event_id = ps.app.charIDToTypeID("Plc ")  # `Plc` needs one space in here.
+        ps.app.executeAction(ps.app.charIDToTypeID("Plc "), desc)
+        # Edit the text layers called first_name and last_name (replace their text to the ones from the request.form)
+        for layer in doc.layers:
+            if layer.kind == ps.LayerKind.TextLayer:
+                if layer.name == 'first_name':
+                    layer.textItem.contents = first_name
+                elif layer.name == 'last_name':
+                    layer.textItem.contents = last_name
 
-    # Edit the text layers called first_name and last_name (replace their text to the ones from the request.form)
-    for layer in doc.layers:
-        if layer.kind == ps.LayerKind.TextLayer:
-            if layer.name == 'first_name':
-                layer.textItem.contents = first_name
-            elif layer.name == 'last_name':
-                layer.textItem.contents = last_name
+       # Save the modified document as a new PSD file
+        opts = ps.ExportOptionsSaveForWeb()
+        # Get the path of the current script
+        script_directory = os.path.dirname(os.path.abspath(__file__))
 
-    print("Input are: " + first_name + " " + last_name)
+        # Specify the filename
+        new_file_path = os.path.join(script_directory, 'new_file.png')
+        active_document = ps.app.activeDocument
+        active_document.exportDocument(new_file_path, ps.ExportType.SaveForWeb, opts)
+        os.startfile(png_file)
 
-    # Make it import the profile image into the PSD file
-    # Import the profile image as a new layer
-
-    # Save the document as a new JPG file
-    new_jpg_file = 'new_' + os.path.splitext(psd_file.filename)[0] + '.jpg'
-    doc.saveAs(new_jpg_file, options=ps.JPEGSaveOptions(quality=12), asCopy=True)
-
-    return 'File has been written with the name ' + new_jpg_file
 
 if __name__ == '__main__':
     app.run(port=3000)
+
